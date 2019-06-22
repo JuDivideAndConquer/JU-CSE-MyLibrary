1a0f057f5d1ea4f2755e6e9570395ac53e626e92
Irin <irinbanerjee@gmail.com>
2019-06-23 03:41:54 +0530

    some more stuff

---
+++ jucsemylib/www/js/index.js
@@ -0,0 +1,165 @@
+//function to show the side navigation bar
+function showSidenav()
+{
+	$('#sidenav').css('transition','0.5s ease')
+	$('#sidenav').css('width','60%');
+	$('.sidenavItems').css('display','block');
+	console.log("menu icon clicked");
+}
+
+//fucntion to collapse the side navigation bar
+function hideSidenav()
+{   
+	$('#sidenav').css('transition','0.5s ease')
+	$('#sidenav').css('width','0');
+	$('.sidenavItems').css('display','none');
+	console.log("sidenav hide icon clicked");
+}
+
+var touchx;
+
+//minimum value of x coordinate in order to trigger showSidenav
+var minXShowSidenav = 5;
+
+//minimum value to x coordinate in order to trigger hideSidenav
+var minXHideSidenav = 100;
+
+//event listeners to enable swiping to show side navigation bar
+document.getElementById('content').addEventListener('touchstart',function(evt)
+{
+	touchx = evt.touches[0].clientX;
+	console.log(touchx);
+});
+
+document.getElementById('content').addEventListener('touchmove',function(evt)
+{
+	var touch = evt.touches[0];
+	console.log(touch.clientX);
+
+	if(touchx<5)
+	{
+		$('#sidenav').css('transition','0.1s ease')
+		$('#sidenav').css('width',touch.clientX);
+	}
+});
+
+document.getElementById('content').addEventListener('touchend',function()
+{
+	if(touchx<minXShowSidenav)
+	{
+		showSidenav();
+	}
+});
+
+// event listeners to enable swiping to collapse side navigation bar
+document.getElementById('sidenav').addEventListener('touchmove',function(evt)
+{
+	var touch = evt.touches[0];
+	console.log(touch.clientX);
+	$('#sidenav').css('transition','0.1s ease')
+	$('#sidenav').css('width',touch.clientX);
+	touchx=touch.clientX;
+});
+
+document.getElementById('sidenav').addEventListener('touchend',function()
+{
+	if(touchx<minXHideSidenav)
+	{
+		hideSidenav();
+	}
+	else
+	{
+		showSidenav();
+	}
+});
+
+/*
+//retrives the login page
+function getLoginPage(callback){
+	var request = new XMLHttpRequest();
+	request.onload = function(){
+		callback(request.responseText);
+	};
+	request.open('GET','./login.html');
+	request.send(null);
+}
+//fucntion to load the login page
+function loadLoginPage(){
+	var content = document.getElementById('content');
+	getLoginPage(function(loginPage){
+		content.innerHTML = loginPage;
+	})
+}
+
+
+//retrives the signup page
+function getSignUpPage(callback){
+	var request = new XMLHttpRequest();
+	request.onload = function(){
+		callback(request.responseText);
+	};
+	request.open('GET','./signUp.html');
+	request.send(null);
+}
+//fucntion to load the signup page
+function loadSignUpPage(){
+	var content = document.getElementById('content');
+	getSignUpPage(function(signUp){
+		content.innerHTML = signUp;
+	})
+	hide_icon();
+	change_text();
+	console.log("signup page loaded");
+}
+*/
+
+//---------------------------------------------------------------------------------
+//function to change text in topBar
+function change_text(text){
+	var topnav=document.getElementById('topBarHome');
+	topnav.innerHTML= text;
+}
+
+function hide_topbar(){
+	$('#topBarHome').css('display','none');
+}
+
+//-----------------------------------------------------------------
+//retrives the page
+function getPage(callback,name){
+	var request = new XMLHttpRequest();
+	request.onload = function(){
+		callback(request.responseText);
+	};
+	request.open('GET','./'+name+'.html');
+	request.send(null);
+}
+//fucntion to load the page
+function loadPage(name,text){
+	var content = document.getElementById('content');
+	getPage(function(home){
+		content.innerHTML = home;
+	},name)
+	change_text(text);
+	hideSidenav();
+
+}
+//main fucntion--------------------------------------------------------
+document.addEventListener('deviceready',function(){
+	var PREF_LOGGED_IN = localStorage.getItem('PREF_LOGGED_IN');
+	var PREF_SIGNED_UP = localStorage.getItem('PREF_SIGNED_UP');
+	if(PREF_SIGNED_UP == null || PREF_SIGNED_UP == 'false'){
+		loadPage('signup','Sign Up');
+		PREF_SIGNED_UP='true';
+	}
+	else if(PREF_LOGGED_IN == null || PREF_LOGGED_IN == 'false'){
+		loadPage('login','Login');
+	}
+	else{
+		loadPage('homescreen','Home');
+
+	}
+
+});
+
+
