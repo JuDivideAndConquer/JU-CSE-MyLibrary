4acac23f93f7337e9e08941ac1650b522e4598f9
Md Sahil <287mdsahil@gmail.com>
2019-06-30 02:00:02 +0530

    Implemented search function

--- jucsemylib/www/js/search.js
+++ jucsemylib/www/js/search.js
@@ -1,45 +1,54 @@
 function displayBooks(data) {
-	var data = JSON.parse(data, true);
-	var results = document.getElementById('results');
-	for (i in data) {
-		console.log(data[i]);
-		var bookDiv = createBookDiv(data[i]);
-		results.appendChild(bookDiv);
-	}
+    var data = JSON.parse(data, true);
+    var results = document.getElementById('results');
+    results.innerHTML = "";
+    for (i in data) {
+        console.log(data[i]);
+        var bookDiv = createBookDiv(data[i]);
+        results.appendChild(bookDiv);
+    }
 }
 
 function createBookDiv(book) {
-	var bookDiv = document.createElement('div');
-	bookDiv.classList.add('bookDiv');
+    var bookDiv = document.createElement('div');
+    bookDiv.classList.add('bookDiv');
 
-	for (item in book) {
+    for (item in book) {
 
-		var label = document.createElement('span');
-		label.classList.add('bookDivLabel');
-		var value = document.createElement('span');
-		value.classList.add('bookDivValue');
+        var label = document.createElement('span');
+        label.classList.add('bookDivLabel');
+        var value = document.createElement('span');
+        value.classList.add('bookDivValue');
 
-		label.innerHTML = item + " : ";
-		if (book[item] != "") {
-			value.innerHTML = book[item];
-			var bookSubDiv = document.createElement('div');
-			bookSubDiv.classList.add('book'+item);
-			bookSubDiv.appendChild(label);
-			bookSubDiv.appendChild(value);
-			bookDiv.appendChild(bookSubDiv);
-		}
-	}
+        label.innerHTML = item + " : ";
+        if (book[item] != "") {
+            value.innerHTML = book[item];
+            var bookSubDiv = document.createElement('div');
+            bookSubDiv.classList.add('book' + item);
+            bookSubDiv.appendChild(label);
+            bookSubDiv.appendChild(value);
+            bookDiv.appendChild(bookSubDiv);
+        }
+    }
 
-	return bookDiv;
+    return bookDiv;
 }
 
-function loadResults(params) {
-	if (params == null) {
-		$.post("http://jucse-mylib.000webhostapp.com/searchAll.php", { d: params })
-			.done(function (data) {
-				console.log(data);
-				//alert(data);
-				displayBooks(data);
-			});
-	}
+function loadResults(keyword) {
+    if (keyword == null) {
+        $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
+            .done(function(data) {
+                console.log(data);
+                //alert(data);
+                displayBooks(data);
+            });
+    } else {
+        console.log(keyword);
+        $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
+            .done(function(data) {
+                console.log(data);
+                //alert(data);
+                displayBooks(data);
+            });
+    }
 }
