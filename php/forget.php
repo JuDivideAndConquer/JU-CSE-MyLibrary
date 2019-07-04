22fdcbda0968c3f0842782b9555178b5bb887f9c
SadrulMolla <mollasumon54@gmail.com>
2019-07-04 22:20:28 +0530

    sending mail using php mailer

--- php/forget.php
+++ php/forget.php
@@ -8,9 +8,9 @@ $mail = new PHPMailer();
 
 $mail->isSMTP();                                    // Set mailer to use SMTP
 $mail->Host = 'smtp1.gmail.com';                    // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;
-$mail->SMTPKeepAlive = true;   						// Enable SMTP authentication
-$mail->Mailer = “smtp”; // don't change the quotes!               
+$mail->SMTPKeepAlive = true;   
+$mail->Mailer = “smtp”; // don't change the quotes!
+$mail->SMTPAuth = true;                             // Enable SMTP authentication
 $mail->Username = EMAIL;                         	// SMTP username
 $mail->Password = PASS;                           	// SMTP password
 $mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
@@ -28,8 +28,6 @@ $mail->isHTML(true);                                 // Set email format to HTML
 $mail->Subject = "OTP";
 $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-echo !extension_loaded('openssl')?"Not Available":"Available";
 
 if(!$mail->send()) {
     echo 'Message could not be sent.';
