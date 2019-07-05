19256ac3690ddb87c32b4b6d9ae89676de0b2b57
Irin <irinbanerjee@gmail.com>
2019-07-05 10:49:54 +0530

    admin_side created

---
+++ user_side/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,138 @@
+//function to show the side navigation bar
+function showSidenav() {
+    $('#sidenav').css('transition', '0.5s ease')
+    $('#sidenav').css('width', '60%');
+    $('.sidenavItems').css('display', 'block');
+    console.log("menu icon clicked");
+}
+
+//fucntion to collapse the side navigation bar
+function hideSidenav() {
+    $('#sidenav').css('transition', '0.5s ease')
+    $('#sidenav').css('width', '0');
+    $('.sidenavItems').css('display', 'none');
+    console.log("sidenav hide icon clicked");
+}
+
+var touchx;
+
+//minimum value of x coordinate in order to trigger showSidenav
+var minXShowSidenav = 5;
+
+//minimum value to x coordinate in order to trigger hideSidenav
+var minXHideSidenav = 100;
+
+//event listeners to enable swiping to show side navigation bar
+document.getElementById('content').addEventListener('touchstart', function(evt) {
+    touchx = evt.touches[0].clientX;
+    console.log(touchx);
+});
+
+document.getElementById('content').addEventListener('touchmove', function(evt) {
+    var touch = evt.touches[0];
+    console.log(touch.clientX);
+
+    if (touchx < 5) {
+        $('#sidenav').css('transition', '0.1s ease')
+        $('#sidenav').css('width', touch.clientX);
+    }
+});
+
+document.getElementById('content').addEventListener('touchend', function() {
+    if (touchx < minXShowSidenav) {
+        showSidenav();
+    }
+});
+
+// event listeners to enable swiping to collapse side navigation bar
+document.getElementById('sidenav').addEventListener('touchmove', function(evt) {
+    var touch = evt.touches[0];
+    console.log(touch.clientX);
+    $('#sidenav').css('transition', '0.1s ease')
+    $('#sidenav').css('width', touch.clientX);
+    touchx = touch.clientX;
+});
+
+document.getElementById('sidenav').addEventListener('touchend', function() {
+    if (touchx < minXHideSidenav) {
+        hideSidenav();
+    } else {
+        showSidenav();
+    }
+});
+
+
+//---------------------------------------------------------------------------------
+//function to change text in topBar
+function change_text(text) {
+    var topnav = document.getElementById('topBarHome');
+    topnav.innerHTML = text;
+}
+
+//function to hide menu icon
+function hide_icon() {
+    var icon = document.getElementById('menuIcon');
+    icon.style.display = "none";
+    var sidenav = document.getElementById('sidenav');
+    sidenav.style.display = "none";
+}
+
+
+//-----------------------------------------------------------------
+//retrives the page
+function getPage(callback, name) {
+    var request = new XMLHttpRequest();
+    request.onload = function() {
+        callback(request.responseText);
+    };
+    request.open('GET', './' + name + '.html');
+    request.send(null);
+}
+//fucntion to load the page
+function loadPage(name, text, data) {
+    var content = document.getElementById('content');
+    getPage(function(home) {
+        content.innerHTML = home;
+    }, name)
+    if (name == 'signUp' || name == 'login') {
+        hide_icon();
+    }
+    change_text(text);
+    hideSidenav();
+
+}
+
+function logout() {
+    localStorage.setItem('PREF_LOGGED_IN', false);
+    loadPage('login', 'Login', null);
+}
+
+//fucntion to load the page
+function loadSearch() {
+	var request = new XMLHttpRequest();
+	request.onload = function () {
+		var content = document.getElementById('content');
+		content.innerHTML = request.responseText;
+		loadResults(null);
+	};
+	request.open('GET', './search.html');
+	request.send(null);
+	hideSidenav();
+	var icon = document.getElementById('menuIcon');
+	icon.style.display = "block";
+	var sidenav = document.getElementById('sidenav');
+	sidenav.style.display = "block";
+	change_text("Available Books")
+}
+
+//main fucntion--------------------------------------------------------
+document.addEventListener('deviceready', function() {
+    var PREF_LOGGED_IN = localStorage.getItem('PREF_LOGGED_IN');
+    var user_data = localStorage.getItem('user_data');
+    if (PREF_LOGGED_IN == null || PREF_LOGGED_IN == 'false') {
+        loadPage('login', 'Login', null);
+    } else {
+        loadHome(user_data);
+
+    }
+});
---
+++ user_side/platforms/android/app/src/main/assets/www/js/search.js
@@ -0,0 +1,59 @@
+function displayBooks(data) {
+    var data = JSON.parse(data, true);
+    var results = document.getElementById('results');
+    results.innerHTML = "";
+    for (i in data) {
+        console.log(data[i]);
+        var bookDiv = createBookDiv(data[i]);
+        results.appendChild(bookDiv);
+    }
+}
+
+function issueBook(book) {
+    alert(book);
+}
+
+function createBookDiv(book) {
+    var bookDiv = document.createElement('div');
+    bookDiv.classList.add('bookDiv');
+
+    for (item in book) {
+
+        var label = document.createElement('span');
+        label.classList.add('bookDivLabel');
+        var value = document.createElement('span');
+        value.classList.add('bookDivValue');
+
+        label.innerHTML = item + " : ";
+        if (book[item] != "") {
+            value.innerHTML = book[item];
+            var bookSubDiv = document.createElement('div');
+            bookSubDiv.classList.add('book' + item);
+            bookSubDiv.appendChild(label);
+            bookSubDiv.appendChild(value);
+            bookDiv.appendChild(bookSubDiv);
+            bookDiv.addEventListener('onclick', issueBook(book[item]));
+        }
+    }
+
+    return bookDiv;
+}
+
+function loadResults(keyword) {
+    if (keyword == null) {
+        $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
+            .done(function(data) {
+                console.log(data);
+                //alert(data);
+                displayBooks(data);
+            });
+    } else {
+        console.log(keyword);
+        $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
+            .done(function(data) {
+                console.log(data);
+                //alert(data);
+                displayBooks(data);
+            });
+    }
+}
