adcba24714775c2d15c2f44c073f0bffd5f60106
Irin <irinbanerjee@gmail.com>
2019-07-08 11:26:19 +0530

    All functional , forgetPassword still left to be integrated

--- user_side/app/platforms/android/app/src/main/assets/www/js/search.js
+++ user_side/app/platforms/android/app/src/main/assets/www/js/search.js
@@ -3,14 +3,46 @@ function displayBooks(data) {
     var results = document.getElementById('results');
     results.innerHTML = "";
     for (i in data) {
-        console.log(data[i]);
         var bookDiv = createBookDiv(data[i]);
+        let book = data[i];
         results.appendChild(bookDiv);
     }
 }
 
+function changeIssuedBooks(x, acess) {
+    var user_data = JSON.parse(x);
+    $.post("http://jucse-mylib.000webhostapp.com/change.php", { is: user_data["books_issued"], cd: user_data["card_number"], ass: acess })
+        .done(function(data) {
+            console.log(data);
+        });
+}
+
 function issueBook(book) {
-    alert(book);
+    console.log(book);
+    var user_data = JSON.parse(localStorage.getItem('user_data'), true);
+    var card = user_data["card_number"];
+    var num = user_data["books_issued"];
+    if (num > 1) {
+        alert("Cannot issue more than 2 books.");
+        return;
+    }
+    var r = confirm("Do you wish to issue this ?");
+    console.log(r);
+    if (r == false) { return; }
+
+    $.post("http://jucse-mylib.000webhostapp.com/issue.php", { ass: book["acession_number"], cd: card })
+        .done(function(data) {
+            if (data == 1) {
+                user_data["books_issued"] = (parseInt(user_data["books_issued"]) + 1);
+                var x = JSON.stringify(user_data);
+                localStorage.setItem('user_data', x);
+                changeIssuedBooks(x, book["acession_number"]);
+                alert("Issue request successful");
+            } else {
+                alert("Book not available. Try again later.");
+            }
+        });
+
 }
 
 function createBookDiv(book) {
@@ -32,10 +64,11 @@ function createBookDiv(book) {
             bookSubDiv.appendChild(label);
             bookSubDiv.appendChild(value);
             bookDiv.appendChild(bookSubDiv);
-            bookDiv.addEventListener('onclick', issueBook(book[item]));
         }
     }
-
+    bookDiv.addEventListener('click', function(event) {
+        issueBook(book);
+    });
     return bookDiv;
 }
 
@@ -43,16 +76,12 @@ function loadResults(keyword) {
     if (keyword == null) {
         $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
             .done(function(data) {
-                console.log(data);
-                //alert(data);
                 displayBooks(data);
             });
     } else {
         console.log(keyword);
         $.post("http://jucse-mylib.000webhostapp.com/search.php", { d: keyword })
             .done(function(data) {
-                console.log(data);
-                //alert(data);
                 displayBooks(data);
             });
     }
