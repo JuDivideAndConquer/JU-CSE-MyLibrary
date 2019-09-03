f54efc47361355d95855abc1e1ff27a677fde579
SadrulMolla <mollasumon54@gmail.com>
2019-09-03 23:52:16 +0530

    Adding mail varification1

--- jucsemylib/www/js/signUp.js
+++ jucsemylib/www/js/signUp.js
@@ -1,12 +1,12 @@
 
-function signUp(cardnumber, name, course, passw, passwc) {
+function signUp(cardnumber, name, course, passw, passwc,mail) {
 	if (passw != passwc) {
 		alert("Passwords do not match");
 		return false;
 	}
 
 
-	$.post("http://jucse-mylib.000webhostapp.com/signUp.php", { cd: cardnumber, nm: name, cs: course, pw: passw })
+	$.post("http://jucse-mylib.000webhostapp.com/signUp.php", { cd: cardnumber, nm: name, cs: course, pw: passw, ml:mail })
 		.done(function (data) {
 			alert(data);
 			loadPage('login','Login');
