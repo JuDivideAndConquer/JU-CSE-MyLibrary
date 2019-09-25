506c15778d87662ea107a6f4e272eeb8a55c2fd5
SadrulMolla <mollasumon54@gmail.com>
2019-09-25 23:13:22 +0530

    adding all tesing branch function

--- user_side/app/www/js/signUp.js
+++ user_side/app/www/js/signUp.js
@@ -1,12 +1,12 @@
 
-function signUp(cardnumber, name, course, passw, passwc,mail) {
+function signUp(cardnumber, name, course, passw, passwc) {
 	if (passw != passwc) {
 		alert("Passwords do not match");
 		return false;
 	}
 
 
-	$.post("http://jucse-mylib.000webhostapp.com/signUp.php", { cd: cardnumber, nm: name, cs: course, pw: passw, ml:mail })
+	$.post("http://jucse-mylib.000webhostapp.com/signUp.php", { cd: cardnumber, nm: name, cs: course, pw: passw })
 		.done(function (data) {
 			alert(data);
 			loadPage('login','Login');
